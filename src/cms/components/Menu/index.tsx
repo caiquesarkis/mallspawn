2f1dddc4df32ddedaf86337abf61358c01d81f77
Caique <caique_sarkis_corebiz@whirlpool.com>
2022-05-26 10:22:18 -0300

    Adding avatar and config button

--- src/cms/components/Menu/index.tsx
+++ src/cms/components/Menu/index.tsx
@@ -1,12 +1,12 @@
-import { Box, HStack, Text, List, ListIcon, ListItem, VStack, Button } from "@chakra-ui/react";
-import { FiArchive, FiBox, FiHome } from "react-icons/fi";
+import { Box, HStack, Text, List, ListIcon, ListItem, VStack, Button, Icon, Heading } from "@chakra-ui/react";
+import { FiArchive, FiBox, FiHome, FiSettings } from "react-icons/fi";
 import { IconType } from "react-icons/lib";
 import { Link } from "react-router-dom";
-
+import { Avatar } from '@chakra-ui/react'
 interface menuLink{
     route: string,
     text: string,
-    icon: IconType
+    icon?: IconType
 }
 
 const MenuLink = (props: menuLink) =>{
@@ -34,16 +34,67 @@ const MenuLink = (props: menuLink) =>{
     )
 }
 
+const ConfigButton = (props: menuLink) =>{
+    const {route, text, icon} = props;
+    return(
+        <Box
+            justifySelf="flex-end"
+        >
+            <Link to={route}>
+                <Icon as={icon} fontSize="3xl" color='gray.300'/>
+            </Link>
+        </Box>
+    )
+}
+
+const CostumerAvatar = (props: {route: string, costumerName:string,  src: string}) =>{
+    const {route, costumerName, src} = props;
+    return(
+        <Box
+            justifySelf="flex-end"
+        >
+            <Link to={route}>
+                <Avatar border='2px' borderColor='gray.300' size='md' name={costumerName} src={src} />
+            </Link>
+        </Box>
+    )
+}
+
 export function Menu(){
     return(
-        <HStack>
-           <VStack bg='gray.900' minW='250px' p={4} h="100vh" color='black' display='flex' justifyContent='center'>
-                <List spacing={4}>
-                    <MenuLink icon={FiHome} route="/" text="Home"/> 
-                    <MenuLink icon={FiArchive} route="/" text="Products"/> 
-                    <MenuLink icon={FiBox} route="/" text="Collections"/> 
+        <HStack 
+        >            
+           <VStack bg='gray.900' minW='250px' p={4} minH="100vh" color='black' display='flex' justifyContent='space-between'>
+                <Link 
+                    to="/"
+                    rel="noopener noreferrer"
+                    style={{"textDecoration": "none"}}
+                >
+                    <Button
+                        color="blue.50"
+                        colorScheme='blue'
+                        mt={8}
+                        variant='ghost'
+                        fontSize="1.5rem"
+                        letterSpacing={6}
+                        textAlign='center'
+                    >
+                        Mallspawn
+                    </Button>
+                </Link>
+                <List spacing={6}>
+                    <MenuLink icon={FiHome} route="/store" text="Home"/> 
+                    <MenuLink icon={FiArchive} route="/store" text="Products"/> 
+                    <MenuLink icon={FiBox} route="/store" text="Collections"/> 
                 </List>
-            </VStack> 
+                <HStack
+                    justifyContent='center' 
+                    gap={12}
+                >
+                    <ConfigButton icon={FiSettings} route="settings" text="Configuration"/> 
+                    <CostumerAvatar route="account" costumerName="Caique Sarkis" src="https://pbs.twimg.com/profile_images/1420483313849016321/fJ856PK9_400x400.jpg"/> 
+                </HStack>
+           </VStack>
         </HStack>
         
     )
