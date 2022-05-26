960a068f58317e32d2de9e5e62ef142a55442604
Caique <caique_sarkis_corebiz@whirlpool.com>
2022-05-26 16:23:25 -0300

    CMS navbar improvements

--- src/cms/components/Menu/index.tsx
+++ src/cms/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Text, List, ListIcon, ListItem, VStack, Button, Icon, Heading } from "@chakra-ui/react";
+import { Box, HStack, Text, List, ListIcon, ListItem, VStack, Button, Icon, Heading, Divider } from "@chakra-ui/react";
 import { FiArchive, FiBox, FiHome, FiSettings } from "react-icons/fi";
 import { IconType } from "react-icons/lib";
 import { Link } from "react-router-dom";
@@ -17,12 +17,12 @@ const MenuLink = (props: menuLink) =>{
             alignItems='center'
             justifyContent='flex-start'
         >
-            <Button  border='2px' borderColor='gray.300'>
-                <Link to={route}>
+            <Button variant="outline" justifyContent='flex-start'  border='2px' minW="100%" borderColor='gray.300'>
+                <Link style={{"display":"flex", "alignItems": "center"}} to={route}>
                     <ListIcon as={icon} fontSize="xl" color='gray.300'/>
                     <Text 
                         as="span" 
-                        fontSize="xl" 
+                        fontSize="md" 
                         fontWeight='600' 
                         color='gray.300'>
                         {text}
@@ -37,13 +37,18 @@ const MenuLink = (props: menuLink) =>{
 const ConfigButton = (props: menuLink) =>{
     const {route, text, icon} = props;
     return(
-        <Box
-            justifySelf="flex-end"
-        >
-            <Link to={route}>
-                <Icon as={icon} fontSize="3xl" color='gray.300'/>
+        <Button variant="outline" justifyContent='flex-start'  border='2px' minW="100%" borderColor='gray.300'>
+            <Link style={{"display":"flex", "alignItems": "center"}} to={route}>
+                <Icon as={icon} fontSize="xl" mr="8px" color='gray.300'/>
+                <Text 
+                    as="span" 
+                    fontSize="md" 
+                    fontWeight='600' 
+                    color='gray.300'>
+                    {text}
+                </Text>
             </Link>
-        </Box>
+        </Button>
     )
 }
 
@@ -53,8 +58,8 @@ const CostumerAvatar = (props: {route: string, costumerName:string,  src: string
         <Box
             justifySelf="flex-end"
         >
-            <Link to={route}>
-                <Avatar border='2px' borderColor='gray.300' size='md' name={costumerName} src={src} />
+            <Link  to={route}>
+                <Avatar size='md' bg='transparent'  name={costumerName} src={src} />
             </Link>
         </Box>
     )
@@ -64,7 +69,7 @@ export function Menu(){
     return(
         <HStack 
         >            
-           <VStack bg='gray.900' minW='250px' p={4} minH="100vh" color='black' display='flex' justifyContent='space-between'>
+           <VStack bg='gray.900' minW='250px' p={8} minH="100vh" color='black' display='flex' justifyContent='space-between'>
                 <Link 
                     to="/"
                     rel="noopener noreferrer"
@@ -75,25 +80,22 @@ export function Menu(){
                         colorScheme='blue'
                         mt={8}
                         variant='ghost'
-                        fontSize="1.5rem"
+                        fontSize="1.2rem"
                         letterSpacing={6}
                         textAlign='center'
                     >
                         Mallspawn
                     </Button>
                 </Link>
-                <List spacing={6}>
+                <List minW='100%' spacing={4}>
                     <MenuLink icon={FiHome} route="/store" text="Home"/> 
                     <MenuLink icon={FiArchive} route="/store" text="Products"/> 
                     <MenuLink icon={FiBox} route="/store" text="Collections"/> 
-                </List>
-                <HStack
-                    justifyContent='center' 
-                    gap={12}
-                >
+                    <Divider borderColor='gray.500'/>
                     <ConfigButton icon={FiSettings} route="settings" text="Configuration"/> 
-                    <CostumerAvatar route="account" costumerName="Caique Sarkis" src="https://pbs.twimg.com/profile_images/1420483313849016321/fJ856PK9_400x400.jpg"/> 
-                </HStack>
+                </List>
+                
+                <CostumerAvatar route="account" costumerName="Caique Sarkis" src="https://pbs.twimg.com/profile_images/1420483313849016321/fJ856PK9_400x400.jpg"/> 
            </VStack>
         </HStack>
         
